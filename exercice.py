e86bb52d9a50f8480e54d24309a931acb6872320
Sebastien Piche <breacham@hotmail.com>
2020-09-24 18:48:17 -0400

    finished

--- exercice.py
+++ exercice.py
@@ -4,24 +4,56 @@
 import math
 
 def get_num_letters(text):
-	return 0
+	num_letters = 0
+	for chr in text:
+		num_letters += int(chr.isalnum())
+	return num_letters
+	# return len([None for chr in text if chr.isalnum()])
 
 def get_word_length_histogram(text):
-	return [0]
+	histogram = [0]
+	for word in text.split():
+		length = get_num_letters(word)
+		if length >= len(histogram):
+			histogram += [0] * (length - len(histogram) +1)
+		histogram[length] += int(length != 0)
+	return histogram
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
 
-	return ""
+	alignment = len(str(len(histogram) - 1))
+	return "\n".join([f"{i : >{alignment}} {ROW_CHAR * elem}" for i, elem in enumerate(histogram) if i != 0])
+	# result = ""
+	# for i, elem in enumerate(histogram):
+	# 	if i == 0:
+	# 		continue
+	# 	result += f"{i : >{aligment}}{ROW_CHAR * elem}" + "\n"
+	# return result
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
 
-	return ""
+	height = max(histogram)
+	result = ""
+	# for i in range(height -1, -1, -1):
+	# 	for elem in histogram[1:]:
+	# 		result += BLOCK_CHAR if elem >= i + 1 else " "
+	# 	result += "\n"
+	# result += LINE_CHAR * len(histogram)
+	# return result
 
+	for i in range(height -1, -1, -1):
+		result += "".join([BLOCK_CHAR if elem >= i + 1 else " " for elem in histogram[1:]]) + "\n"
+	result += LINE_CHAR * len(histogram)
+	return result
 
 if __name__ == "__main__":
+	print(get_num_letters("Hello"))
+	print(get_word_length_histogram("hello, you"))
+
+	print("---------------------------------------------")
 	spam = "Stop right there criminal scum! shouted the guard confidently."
 	eggs = get_word_length_histogram(spam)
 	print(eggs, "\n")
